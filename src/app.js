4af3f47b1b442ffff3b2db7818cfa50fc49005de
Dave-Patsy <dgwh1995@hotmail.com>
2024-07-09 21:08:28 -0700

    added cors configuration

--- src/app.js
+++ src/app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require("cors");
 const todosRouter = require('./routes/todos');
 
 const app = express();
 
+const corsOptions = {
+  // origin: "https://t3-portfolio-a.vercel.app",
+  origin: "*",
+};
+
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
