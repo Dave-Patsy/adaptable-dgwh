1060f6a5480f7a46f351d27306b18cc795ae2b4d
Dave-Patsy <dgwh1995@hotmail.com>
2024-07-09 21:43:19 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use('/todos', todosRouter);
 app.get('/test',(req,res)=>{
   const origin =
     req.headers.origin || req.headers.referer || "Origin not found";
-  res.send(`Request Origin: ${origin}`);
+  res.json(`Request Origin: ${origin}`);
 })
 
 
